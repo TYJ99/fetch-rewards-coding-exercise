5696722f04cf4ccd60af7d24b7d03d75d74d7659
Yen-Ju Tseng <ytseng@ucsd.edu>
2024-03-12 05:04:25 -0500

    Removed unused dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,14 +103,9 @@ dependencies {
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
-//    implementation("com.google.dagger:hilt-android:2.40.5")
-//    kapt ("com.google.dagger:hilt-android-compiler:2.40.5")
-//    implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     kapt ("androidx.hilt:hilt-compiler:1.2.0")
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-    // Location Services
-    // implementation ("com.google.android.gms:play-services-location:21.0.1")
 
 
     // Retrofit
@@ -122,12 +117,6 @@ dependencies {
 
 
 
-    /*
-    // KotlinX Serialization
-    //implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-
-     */
 
     // Compose dependencies
     // ViewModel
@@ -137,69 +126,9 @@ dependencies {
     // Compose Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    // FlowRow
-    // implementation ("com.google.accompanist:accompanist-flowlayout:0.17.0")
-
-    /*
-    // Paging 3.0
-    val paging_version = "3.2.1"
-    implementation("androidx.paging:paging-runtime-ktx:$paging_version")
-    // implementation("androidx.paging:paging-compose:3.3.0-alpha02")
-    implementation ("androidx.paging:paging-compose:1.0.0-alpha14")
-
-     */
-
-    /*
-    // Room Database
-    val roomVersion: String = "2.5.2"
-
-
-    // Room and Lifecycle dependencies
-    implementation("androidx.room:room-runtime:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
-    // kotlin extensions for coroutine support with room
-    implementation("androidx.room:room-ktx:$roomVersion")
-    implementation ("androidx.room:room-paging:$roomVersion")
-
-     */
     val activityVersion: String = "1.8.2"
     // kotlin extensions for coroutine support with activities
     implementation("androidx.activity:activity-ktx:$activityVersion")
-
-    // Coil
-    //implementation("io.coil-kt:coil-compose:1.3.2")
-
-    /*
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    //ksp("com.github.bumptech.glide:ksp:4.11.0")
-    //implementation("com.github.bumptech.glide:glide:4.11.0")
-    kapt("com.github.bumptech.glide:compiler:4.15.1")
-    //ksp("com.github.bumptech.glide:ksp:4.15.1")
-
-     */
-
-    /*
-    // Timber
-    implementation("com.jakewharton.timber:timber:4.7.1")
-
-     */
-
-    /*
-    // Firebase Firestore
-//    implementation("com.google.firebase:firebase-firestore:24.9.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.3")
-
-    // Firebase Storage KTX
-    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
-
-
-
-    // Firebase Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
-
-     */
-
 
     // splash screen
     implementation("androidx.core:core-splashscreen:1.0.1")
